56fc8cf8e95fc0a61a5beb5f321339fac2c8e951
Rek <rekkisomo@pm.me>
2025-02-13 02:26:26 +1100

    Change default API URL
    Signed-off-by: Rek <rekkisomo@pm.me>

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import {Treaty, treaty} from "@elysiajs/eden";
-import {Packbase} from "@/routes";
-import {createClient, SupabaseClient} from "@supabase/supabase-js";
+import {Treaty, treaty} from '@elysiajs/eden'
+import {Packbase} from '@/routes'
+import {createClient, SupabaseClient} from '@supabase/supabase-js'
 
 export default class VoyageSDK {
-    baseURL: string;
+    baseURL: string
     vg: Treaty.Create<Packbase> & {
         supabase?: SupabaseClient<any, any, any>;
-    };
+    }
 
-    constructor(baseURL: string = 'https://shrew.api.packbase.app', options?: {
+    constructor(baseURL: string = 'https://shrew-api.packbase.app', options?: {
         token?: string;
         supabase: {
             client?: any;
@@ -16,12 +16,12 @@ export default class VoyageSDK {
             URL?: string;
         }
     }) {
-        this.baseURL = baseURL;
+        this.baseURL = baseURL
         this.vg = treaty<Packbase>(this.baseURL, {
             headers: {
                 authentication: `Bearer ${options?.token || ''}`
             }
-        });
+        })
 
         if (options?.supabase) {
             const {client, key, URL} = options.supabase
